3a82e218c4e7fdce4e68c8ced3d44dd083888fd7
Mozafar <mozafar@dhis2.org>
2024-09-28 17:17:10 +0100

    fix: update d2.config (#8)
    BREAKING CHANGE: publish v100 to apphub

--- d2.config.js
+++ d2.config.js
@@ -1,7 +1,10 @@
 const config = {
+    id: '1aae7392-f21e-4d6c-ad4b-90b09257d637',
     type: 'app',
     name: 'route-manager',
     title: 'Route Manager',
+
+    minDHIS2Version: '2.40',
 
     entryPoints: {
         app: './src/App.tsx',
