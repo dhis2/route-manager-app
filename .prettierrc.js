d63d5b8ce5d6e27abb31f9ed047299100becf4e4
Mozafar Haider <mozafar@dhis2.org>
2025-12-01 23:34:32 +0000

    chore: move to eslint and  prettier shared configs

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { config } = require('@dhis2/cli-style')
-
-module.exports = {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    ...require(config.prettier),
-}
